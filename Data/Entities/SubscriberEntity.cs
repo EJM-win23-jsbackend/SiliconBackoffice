09d97bed6ed26ad43f801a4b749c24992a3360ce
Marcus <Marcuswix@yahoo.se>
2024-05-13 16:13:32 +0200

    "Subscribe-design-done"

---
+++ Data/Entities/SubscriberEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities
+{
+    public class SubscriberEntity
+    {
+        [Key]
+        public string Id {  get; set; } = Guid.NewGuid().ToString();    
+
+        [Required]
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ EJM-Silicon-Backoffice/EJM-Silicon-Backoffice/Models/SubscribeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJMSiliconBackoffice.Models
+{
+    public class SubscribeModel
+    {
+        [StringLength(100, ErrorMessage ="Email is to long, make it shorter!")]
+        [Display(Name = "Email", Prompt = "Your Email", Order = 0)]
+        [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid Email")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+    }
+}
